6e6c9d3a01c30a011170bd287754716f5ec5b892
Chameleon Cloud User <cc@flash-inst2.novalocal>
2023-07-20 12:38:38 +0000

    complete impl of v1.cpp; works on cham

--- src/v1.cpp
+++ src/v1.cpp
@@ -5,10 +5,10 @@
 #include <mutex>
 #include <string>
 #include <semaphore>
+#include <cassert>
 
 
 
-// std::binary_semaphore hi;
 const size_t max_empty = 10;
 std::counting_semaphore full{0}, empty{max_empty};
 std::queue<void *> ready;
@@ -20,8 +20,6 @@ void *producer_thread(void *arg)
     uint32_t i = 0;
     while (true) {
 
-        ready.pop();
-
         // printf("A\n");
         empty.acquire();
         uint32_t *new_val = (uint32_t *)malloc(sizeof(int));
@@ -31,8 +29,8 @@ void *producer_thread(void *arg)
         ready_guard.lock();
         ready.push(new_val);
         assert(*((uint32_t *) ready.back()) == *new_val);
-        printf("PROD %u\n", *new_val);
         ready_guard.unlock();
+        printf("PROD %u\n", *new_val);
 
         full.release();
     }
@@ -42,6 +40,17 @@ void *producer_thread(void *arg)
 void *consumer_thread(void *arg)
 {
     while (true) {
+
+        // printf("A\n");
+        full.acquire();
+
+        ready_guard.lock();
+        uint32_t *popped_val = (uint32_t *)ready.front();
+        ready.pop();
+        ready_guard.unlock();
+        printf("CONS %u\n", *popped_val);
+
+        empty.release();
         
     }
 
@@ -49,7 +58,13 @@ void *consumer_thread(void *arg)
 
 int main(int argc, char *argv[]) 
 {
-    std::counting_semaphore hi;
+    std::thread p1(producer_thread, nullptr);
+    std::thread c1(consumer_thread, nullptr);
+    std::thread c2(consumer_thread, nullptr);
+
+    p1.join();
+    c1.join();
+    c2.join();
 }
 
 
