90253745121e595f8ccc92ec86f76b5fe3a818e5
Chameleon Cloud User <cc@flash-inst2.novalocal>
2023-07-21 20:33:08 +0000

    added measuring; https://stackoverflow.com/questions/53708076/what-is-the-proper-way-to-use-clock-gettime

--- src/v1.c
+++ src/v1.c
@@ -125,8 +125,23 @@ void *consumer_thread(void *arg)
 }
 
 
-
-
+#include <time.h>
+enum { NS_PER_SECOND = 1000000000 };
+void sub_timespec(struct timespec t1, struct timespec t2, struct timespec *td)
+{
+    td->tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    td->tv_sec  = t2.tv_sec - t1.tv_sec;
+    if (td->tv_sec > 0 && td->tv_nsec < 0)
+    {
+        td->tv_nsec += NS_PER_SECOND;
+        td->tv_sec--;
+    }
+    else if (td->tv_sec < 0 && td->tv_nsec > 0)
+    {
+        td->tv_nsec -= NS_PER_SECOND;
+        td->tv_sec++;
+    }
+}
 
 int main(int argc, char *argv[]) 
 {
@@ -147,7 +162,6 @@ int main(int argc, char *argv[])
     // assert (empty != SEM_FAILED);
     // assert (list_guard != SEM_FAILED);
 
-
     sem_init(&full, 0, 0);
     sem_init(&empty, 0, BUFFER_SIZE);
     sem_init(&list_guard, 0, 1);
@@ -163,6 +177,9 @@ int main(int argc, char *argv[])
 
     pthread_t producers[num_producers];
     pthread_t consumers[num_consumers];
+
+    struct timespec start, finish, delta;
+    clock_gettime(CLOCK_REALTIME, &start);
     for (size_t i = 0; i < num_producers; ++i)
         pthread_create(&producers[i], NULL, producer_thread, NULL);
     for (size_t i = 0; i < num_consumers; ++i)
@@ -175,5 +192,9 @@ int main(int argc, char *argv[])
     for (size_t i = 0; i < BUFFER_SIZE; ++i)
         sem_wait(&empty);
     /* if all buffer blocks are empty, all consumers must have finished */
+
+    clock_gettime(CLOCK_REALTIME, &finish);
+    sub_timespec(start, finish, &delta);
+    printf("%d.%.9ld\n", (int)delta.tv_sec, delta.tv_nsec);
     return 0;
 }
--- src/v2.c
+++ src/v2.c
@@ -147,6 +147,23 @@ void *consumer_thread(void *arg)
 }
 
 
+#include <time.h>
+enum { NS_PER_SECOND = 1000000000 };
+void sub_timespec(struct timespec t1, struct timespec t2, struct timespec *td)
+{
+    td->tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    td->tv_sec  = t2.tv_sec - t1.tv_sec;
+    if (td->tv_sec > 0 && td->tv_nsec < 0)
+    {
+        td->tv_nsec += NS_PER_SECOND;
+        td->tv_sec--;
+    }
+    else if (td->tv_sec < 0 && td->tv_nsec > 0)
+    {
+        td->tv_nsec -= NS_PER_SECOND;
+        td->tv_sec++;
+    }
+}
 
 int main(int argc, char *argv[]) 
 {
@@ -180,6 +197,10 @@ int main(int argc, char *argv[])
 
     pthread_t producers[num_producers];
     pthread_t consumers[num_consumers];
+
+    struct timespec start, finish, delta;
+    clock_gettime(CLOCK_REALTIME, &start);
+
     for (size_t i = 0; i < num_producers; ++i)
         pthread_create(&producers[i], NULL, producer_thread, NULL);
     for (size_t i = 0; i < num_consumers; ++i)
@@ -192,5 +213,8 @@ int main(int argc, char *argv[])
     for (size_t i = 0; i < BUFFER_SIZE; ++i)
         sem_wait(&empty);
     /* if all buffer blocks are empty, all consumers must have finished */
+    clock_gettime(CLOCK_REALTIME, &finish);
+    sub_timespec(start, finish, &delta);
+    printf("%d.%.9ld\n", (int)delta.tv_sec, delta.tv_nsec);
     return 0;
 }
