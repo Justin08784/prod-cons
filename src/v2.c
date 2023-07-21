60c01ef60644b523d3bf86ea6e803162a8acb851
Chameleon Cloud User <cc@flash-inst2.novalocal>
2023-07-21 10:27:43 +0000

    added prod limit

--- src/v2.c
+++ src/v2.c
@@ -13,17 +13,23 @@
 #include <utlist.h>
 #include <utarray.h>
 
-#define BUFFER_SIZE 16
-#define BLOCK_SIZE 16
-#define ENABLE_DEBUG true
-
-
 typedef enum status {
     EMPTY = 0b00,
     EMPTYING = 0b01,
     FILLING = 0b10,
     FULL = 0b11
 } status_t;
+
+#define BUFFER_SIZE 16 // in blocks
+#define BLOCK_SIZE 16 // in bytes
+#define ENABLE_DEBUG true
+#define PROD_LIMIT 10000 // max # of production cycles
+
+void *buffer[BUFFER_SIZE];
+status_t free_map[BUFFER_SIZE];
+sem_t full;
+sem_t empty;
+sem_t buff_guard;
 
 
 inline void *ck_malloc(size_t size)
@@ -34,20 +40,12 @@ inline void *ck_malloc(size_t size)
     return rv;
 }
 
-void *buffer[BUFFER_SIZE];
-status_t free_map[BUFFER_SIZE];
-sem_t full;
-sem_t empty;
-sem_t buff_guard;
-
-
 inline int find_free()
 {
     sem_wait(&buff_guard);
     for (size_t free_index = 0; free_index < BUFFER_SIZE; ++free_index) {
         if (free_map[free_index] == EMPTY) {
             free_map[free_index] = FILLING;
-            // printf("%x\n", free_map[0]);
             sem_post(&buff_guard);
             return free_index;
         }
@@ -63,7 +61,6 @@ inline int find_full()
     for (size_t full_index = 0; full_index < BUFFER_SIZE; ++full_index) {
         if (free_map[full_index] == FULL) {
             free_map[full_index] = EMPTYING;
-            // printf("%x\n", free_map[0]);
             sem_post(&buff_guard);
             return full_index;
         }
@@ -103,22 +100,20 @@ void produce(void *arg, void *out_dst, size_t *out_bytes)
 
 void *producer_thread(void *arg)
 {
-    for (;;) {
+    for (size_t cycle_no = 0; cycle_no < PROD_LIMIT; ++cycle_no) {
         size_t bytes;
-
 
         sem_wait(&empty);
 
         int free_index = find_free();
-        produce(arg, buffer[free_index], &bytes);
-        if (ENABLE_DEBUG) {
-            uint32_t *val = (uint32_t *) buffer[free_index];
-            printf("PROD %u\n", *val);
-        }
+        produce(NULL, buffer[free_index], &bytes);
+        if (ENABLE_DEBUG)
+            printf("PROD %u\n", *((uint32_t *) buffer[free_index]));
+        
         mark_as_full(free_index);
-
         sem_post(&full);
     }
+
 
 }
 
@@ -134,12 +129,10 @@ void *consumer_thread(void *arg)
         void *val = buffer[full_index];
         if (ENABLE_DEBUG)
             printf("CONS %u\n", *((uint32_t *) val));
-        
         rand_r(&rand_state);
         consume(val);
 
         mark_as_empty(full_index);
-
         sem_post(&empty);
     }
 }
